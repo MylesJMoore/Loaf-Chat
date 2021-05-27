9d4527c61f79399609d3b07b6f20495cde8fa151
Myles Moore <mylesjmoore@gmail.com>
2021-05-26 19:14:08 -0700

    Created chat feed with backgrounds for sent messages. Still pending read-receipts of other people.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="b0f1354a-4abf-44a0-a778-e7ea93de8bb9"
+            userName="Loafboy"
+            userSecret="loafcentral1"
+            renderChatFeed = {(chatAppProps) => <ChatFeed {... chatAppProps} />}
+        />
+    )
 }
 
 export default App;
